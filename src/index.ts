d525e5a060c7e14b215534efd5b495ea51382852
Dmitry Ivanov <dimannisik1@gmail.com>
2023-06-21 14:14:28 +0700

    feat: add CORS

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,15 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
   const { pathname } = new URL(`http://${req.headers.host}${req.url}`);
 
+  res.setHeader('Access-Control-Allow-Origin', process.env.ORIGIN || 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
+
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return;
+  }
+
   if (pathname === '/users') {
     if (req.method === 'POST') {
       handleRequest(searchUsers, req, res);
